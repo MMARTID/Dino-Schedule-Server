55546581d867576bebab05b35a5c00e97f111e40
MMARTID <miguelmartindelgado1998@gmail.com>
2025-06-19 16:44:42 +0200

    model & get-post routes for client coll

--- models/Client.model.js
+++ models/Client.model.js
@@ -2,7 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const ClienteSchema = new Schema({
   uid:   { type: String, required: true, unique: true }, //! Ref para RRHH
-  nombre:{ type: String, required: true }
+  nombre:{ type: String, required: true },
+  titulo:{ type: String, required: true },
 });
 const Cliente = model('Cliente', ClienteSchema);
 module.exports = Cliente;
--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,2 +1,33 @@
 const router = require("express").Router();
+const Cliente = require("../models/Client.model");
+
+// TODOS LOS CLIENTES:
+//! http://localhost:5005/api/clients/
+router.get("/", async (req, res, next) => {
+  try {
+    const clients = await Cliente.find();
+    res.status(200).json(clients);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// AÑADIR UN CLIENTE:
+//! http://localhost:5005/api/clients/
+router.post('/', async (req, res, next) => {
+    const { uid, nombre, titulo } = req.body;
+    try {
+        const newClient = new Cliente({ uid, nombre, titulo });
+        await newClient.save();
+        res.status(201).json("Client added successfully");
+    } catch (error) {
+        next(error);
+    }
+})
+
+// BORRAR UN CLIENTE:
+//! http://localhost:5005/api/clients/:clientId
+router.delete('/:clientId', async (req, res, next) => {
+    
+})
 module.exports = router;
